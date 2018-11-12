f998990b4df4c825839bda318df4529a8fcd70c0
SALMAN KHAN <greate43.sk@gmail.com>
2018-11-12 14:00:13 +0500

    Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {User} = require('./model/user');
 var {Todo} = require('./model/todo');
 
+var {ObjectID}= require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -28,6 +30,24 @@ app.get('/todos',(req,res)=>{
     },(e)=>{
       res.status(400).send(e);
    });
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+ // console.log(id);
+   if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+   }
+
+  Todo.findById(id).then((todo)=>{
+      if(!todo){
+        return res.status(404).send();
+      }
+
+    res.send({todo});
+  },(e)=>{
+    res.status(400).send(e);
+ });
 });
 
 app.listen(3000, () => {
